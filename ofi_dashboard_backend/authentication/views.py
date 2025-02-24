4fc577ad2a58a4ac59a4e78cdabe90c379a45ddf
Stefano Lacorazza <s.lacorazza@ofiservices.com>
2025-02-24 14:48:01 -0500

    Add authentication views and update settings and URLs for user login, signup, and logout

--- ofi_dashboard_backend/authentication/views.py
+++ ofi_dashboard_backend/authentication/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.views import APIView as ApiView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class login(ApiView):
+    def get(self, request):
+        return Response({"message": "Login"})
+    
+class signup(ApiView):
+    def get(self, request):
+        return Response({"message": "Signup"})
+
+class logout(ApiView):
+    def get(self, request):
+        return Response({"message": "Logout"})
