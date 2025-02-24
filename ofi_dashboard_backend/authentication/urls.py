a41db859f1c512bfff66629dac40e76ea561e590
Stefano Lacorazza <s.lacorazza@ofiservices.com>
2025-02-24 14:54:00 -0500

    Update authentication URLs to use class-based views for login, logout, and signup

--- ofi_dashboard_backend/authentication/urls.py
+++ ofi_dashboard_backend/authentication/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    path('signup/', views.signup, name='signup'),
+    path('login/', views.login.as_view(), name='login'),
+    path('logout/', views.logout.as_view(), name='logout'),
+    path('signup/', views.signup.as_view(), name='signup'),
 ]
