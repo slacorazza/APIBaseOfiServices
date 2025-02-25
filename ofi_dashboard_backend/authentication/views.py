f4a254d4514af3df2cacf130645e90844891d77e
Stefano Lacorazza <s.lacorazza@ofiservices.com>
2025-02-25 05:21:26 -0500

    Add serializers for authentication

--- ofi_dashboard_backend/authentication/views.py
+++ ofi_dashboard_backend/authentication/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView as ApiView
 from rest_framework.response import Response
-
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 class login(ApiView):
     def get(self, request):
