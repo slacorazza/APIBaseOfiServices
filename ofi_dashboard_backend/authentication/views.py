790a9d1c5420d66a7b01c2ef2aab2fa6c1775887
Stefano Lacorazza <s.lacorazza@ofiservices.com>
2025-02-25 05:39:28 -0500

    Implement POST method for login view with error handling and token generation

--- ofi_dashboard_backend/authentication/views.py
+++ ofi_dashboard_backend/authentication/views.py
@@ -5,9 +5,22 @@ from .serializers import UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from django.shortcuts import get_object_or_404
+
 
 class login(ApiView):
-    def get(self, request):
+    def post(self, request):
+        try:
+            user = get_object_or_404(User, username=request.data['username'])
+            if not user.check_password(request.data['password']):
+                return Response({"detail": 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+            
+            token, created = Token.objects.get_or_create(user=user)
+            serializer = UserSerializer(instance = user)
+            return Response({"token": token.key}, status=status.HTTP_202_ACCEPTED)
+        except Exception as e:
+            print(f"Error during login: {e}")
+            return Response({"error": "Internal Server Error"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response({"message": "Login"})
     
 class signup(ApiView):
